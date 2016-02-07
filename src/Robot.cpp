d1abcf89466d99b3f175d05b40212ed8515d3c00
Donovan <cplusdragon@gmail.com>
2016-02-06 21:39:49 -0600

    Added Base Code
    Put code for Auto,Teleop,Climber,Launcher,Intake and tried to establish
    working limit switches

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -6,36 +6,51 @@ class Robot: public IterativeRobot
 	RobotDrive myRobot; // robot drive system
 	Joystick stick; // only joystick
 	LiveWindow *lw;
-	Encoder EncDist;
-	Gyro gyro;
+	Encoder EncDist,lEnc,rEnc;
+	AnalogGyro gyro;
+	DigitalInput Autostop,captured,loaded,lSwitch,rSwitch;
+	bool intup,intdown;
+	bool toggle,latch;
+	VictorSP launcher,intake;
+	CanTalonSRX actintake,sm2kl,sm2kr;
 	int autoLoopCounter;
 	float xvalue;
-	float yvalue,motorspeed;
+	float yvalue;
+	float motorspeed;
 	float cir;
 	int Auto,ang;
-	int Rev,d1,d2,d3;
-	float EncVal1,EncVal2,EncVal3;
+	int Rev,d1;
+	float EncVal1;
 
 
 public:
 	void RobotInit() override {
-		//CameraServer::GetInstance()->SetQuality(50);
-		//std::shared_ptr<USBCamera> camera(new USBCamera("cam0", false));
-		//camera->SetExposureManual(100); // change this value
-		//camera->SetBrightness(1); // change this value
-		//CameraServer::GetInstance()->StartAutomaticCapture(camera);
-
-		//Cam.StartCapture();
-
 	    CameraServer::GetInstance()->SetQuality(100);
 		CameraServer::GetInstance()->StartAutomaticCapture("cam0");
+		captured = new DigitalInput(0);
 	}
 	Robot() :
 		myRobot(1, 0),	// these must be initialized in the same order
 		stick(5),		// as they are declared above.
 		lw(LiveWindow::GetInstance()),
-		EncDist(0),
-		gyro(1),
+		EncDist(0,1),
+		lEnc(0,3),
+		rEnc(4,6),
+		gyro(0),
+		Autostop(1),
+		captured(0),
+		loaded(0),
+		lSwitch(0),
+		rSwitch(0),
+		intup(0),
+		intdown(0),
+		toggle(0),
+		latch(0),
+		launcher(5),
+		intake(0),
+		actintake(18),
+		sm2kl(3),
+		sm2kr(5),
 		autoLoopCounter(0),
 		xvalue(0),
 		yvalue(0),
@@ -45,11 +60,7 @@ public:
 		ang(90),
 		Rev(360),
 		d1(12),
-		d2(12),
-		d3(12),
-		EncVal1(0),
-		EncVal2(0),
-		EncVal3(0)
+		EncVal1(0)
 	{
 		myRobot.SetExpiration(0.1);
 	}
@@ -58,6 +69,7 @@ private:
 	void AutonomousInit()
 	{
 		gyro.Reset();
+
 		if(Auto==1)
 		{
 			d1=84;
@@ -79,45 +91,131 @@ private:
 
 		autoLoopCounter = 0;
 		EncVal1=(d1/cir)*Rev;
-		EncVal2=(d2/cir)*Rev;
-		EncVal3=(d3/cir)*Rev;
 
 	}
 
 	void AutonomousPeriodic()
 	{
-		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
-		{
-			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
-			autoLoopCounter++;
-			} else {
-			myRobot.Drive(0.0, 0.0); 	// stop robot
-		}
 		if((Auto==1)||(Auto==2))
 			{
-					while(gyro.GetAngle()<ang)
-					{
-						abs(gyro.GetAngle()-ang)*(1/90)=motorspeed;
-						myRobot.TankDrive(motorspeed/2,motorspeed/-2);
+				while(gyro.GetAngle()<ang)
+				{
+					motorspeed=(1/90)*abs(gyro.GetAngle()-ang);
+					myRobot.TankDrive(motorspeed/2,motorspeed/-2);
 				}
 			}
+		if((Auto==3)||(Auto==4))
+		{
+			while(gyro.GetAngle()>ang)
+			{
+				motorspeed=(1/90)*abs(gyro.GetAngle()-ang);
+				myRobot.TankDrive(motorspeed/-2,motorspeed/2);
+			}
+		}
 		while(EncDist.GetDistance()<EncVal1)
 		{
 			myRobot.Drive(-0.5, 0.0);
 		}
+		ang = -ang;
+		if((Auto==1)||(Auto==2))
+		{
+			while(gyro.GetAngle()<ang)
+			{
+				motorspeed=(1/90)*abs(gyro.GetAngle()-ang);
+				myRobot.TankDrive(motorspeed/2,motorspeed/-2);
+			}
+		}
+		if((Auto==3)||(Auto==4))
+		{
+			while(gyro.GetAngle()>ang)
+			{
+				motorspeed=(1/90)*abs(gyro.GetAngle()-ang);
+				myRobot.TankDrive(motorspeed/-2,motorspeed/2);
+			}
+		}
+		launcher.Set(1.0);
+		while(!Autostop.Get())
+		{
+			myRobot.Drive(-0.5,0.0);
+		}
+		myRobot.Drive(0.0,0.0);
+		intake.Set(1.0);
 
 	}
 
 	void TeleopInit()
 	{
-
+		toggle=false;
+		latch=false;
+		SmartDashboard::init();
+		SmartDashboard::PutNumber("Tests",0);
 	}
 
 	void TeleopPeriodic()
 	{
+		if(stick.GetRawButton(3))
+		{
+			sm2kl.Set(1.0);
+			sm2kr.Set(1.0);
+		}
+		if(lSwitch.Get()||rSwitch.Get())
+		{
+			sm2kr.Set(0.0);
+			sm2kl.Set(0.0);
+			SmartDashboard::PutNumber("Tests",675);
+		}
+		if(stick.GetRawButton(5))
+		{
+			sm2kl.Set(-1.0);
+			sm2kr.Set(-1.0);
+			SmartDashboard::PutNumber("Tests",675);
+		}
+		if((lEnc.GetDistance() > 1080)||(rEnc.GetDistance() > 1080))
+		{
+			sm2kl.Set(0.0);
+			sm2kr.Set(0.0);
+		}
+		if(stick.GetRawButton(10))
+		{
+			actintake.Set(1.0);
+		}
+		else if(stick.GetRawButton(9))
+		{
+			actintake.Set(-1.0);
+		}
+		else
+		{
+			actintake.Set(0.0);
+		}
+
 		xvalue = -stick.GetZ()*.5;
 		yvalue = -stick.GetY();
+		if(stick.GetRawButton(7)&&!toggle)
+		{
+			latch=!latch;
+			toggle=true;
+		}
+		if(!stick.GetRawButton(7)&&toggle)
+		{
+			toggle=false;
+		}
+		if(latch)
+		{
+			yvalue=-yvalue;
+		}
+		if(stick.GetRawButton(1))
+		{
+			launcher.Set(1.0);
+		}
+		if(stick.GetRawButton(11))
+		{
+			intake.Set(1.0);
+		}
 		myRobot.ArcadeDrive(yvalue,xvalue);
+		SmartDashboard::PutNumber("Captured",captured.Get());
+		SmartDashboard::PutBoolean("Loaded",loaded.Get());
+		SmartDashboard::PutBoolean("latch",latch);
+		SmartDashboard::PutNumber("Button",stick.GetRawButton(7));
 		//myRobot.ArcadeDrive(stick); // drive with arcade style (use right stick)
 	}
 
